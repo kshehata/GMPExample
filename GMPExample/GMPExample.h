74b7ffaaf4845600479ced7fb82288af4ab3ca77
Kareem Shehata <kareem@shehata.ca>
2022-03-30 15:07:12 +0800

    Initial Commit

---
+++ GMPExample/GMPExample.h
@@ -0,0 +1,12 @@
+//
+//  GMPExample.h
+//  GMPExample
+//
+//  Created by Kareem Shehata on 30/3/22.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GMPExample : NSObject
+
+@end
---
+++ GMPExample/GMPExample.m
@@ -0,0 +1,12 @@
+//
+//  GMPExample.m
+//  GMPExample
+//
+//  Created by Kareem Shehata on 30/3/22.
+//
+
+#import "GMPExample.h"
+
+@implementation GMPExample
+
+@end
